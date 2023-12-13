b7a0d0ff20c4ca95fd76459b9b1c39987a50d6b4
Pbflex <bellopeter22291@gmail.com>
2023-12-13 15:53:40 -0800

    Betty Correction

--- getline.c
+++ getline.c
@@ -14,7 +14,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 	ssize_t r = 0;
 	size_t len_p = 0;
 
-	if (!*len) /* if nothing left in the buffer, fill it */
+	if (!*len)
 	{
 		/*bfree((void **)info->cmd_buf);*/
 		free(*buf);
@@ -68,7 +68,7 @@ ssize_t get_input(info_t *info)
 		j = i; /* init new iterator to current buf position */
 		p = buf + i; /* get pointer for return */
 
-		check_chain(info, buf, &j, i, len);check_chain(info, buf, &j, i, len);
+		check_chain(info, buf, &j, i, len);
 		while (j < len) /* iterate to semicolon or end */
 			check_chain(info, buf, &j, i, len);
 		while (j < len) /* iterate to semicolon or end */
