20cd1649594b5b7c13fc6c8a78cc6a1dfd28b9b7
Pbflex <bellopeter22291@gmail.com>
2023-12-15 19:02:05 -0800

    Exit Task

--- exit_cmd.c
+++ exit_cmd.c
@@ -10,7 +10,17 @@
 
 void exit_cmd(char **command, char *line)
 {
+	if (command[1] != NULL)
+	{
+		int status = atoi(command[1]);
+		free(line);
+		free_buffers(command);
+		exit(status);
+	}
+	else
+	{
 	free(line);
 	free_buffers(command);
 	exit(EXIT_SUCCESS);
+	}
 }
